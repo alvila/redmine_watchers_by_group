e0fdbad6ddf332545e02621cc3e918d250eb1505
Toshi MARUYAMA <marutosijp2@gmail.com>
2012-10-09 11:31:23 +0900

    Rails3 compatible: use render_on on hook
    Redmine issue 11105 fixed "content_for :header_tags" inside hook.
    http://www.redmine.org/issues/11105
    But, it does not work on :render_to_string.
    It need to use "render_on".

--- lib/hooks/hooks.rb
+++ lib/hooks/hooks.rb
@@ -1,15 +1,5 @@
 module Redmine_watchers_by_group
   class Hooks < Redmine::Hook::ViewListener
-    def view_issues_form_details_bottom(context={ })
-      controller = context[:controller]
-      return '' unless controller
-      action_name = controller.action_name
-      return '' unless action_name
-      return '' unless action_name=="new"
-      context[:controller].send(:render_to_string, {
-          :partial => 'watchers/multiselect_group',
-          :locals => context
-        })
-    end
+    render_on :view_issues_form_details_bottom, :partial => 'watchers/multiselect_group'
   end
 end
